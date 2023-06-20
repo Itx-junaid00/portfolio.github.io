1b6371df79cfc96a95abc1d6cdc48e9a9222f4b8
Itx-junaid00 <junaid0khattar@gmail.com>
2023-06-20 12:44:49 +0500

    my portfolio website

---
+++ index.js
@@ -0,0 +1,25 @@
+var typed = new Typed('#element', {
+    strings: [' PortFolio','Website','of' ,'Junaid'],
+    typeSpeed: 50,
+  });
+  document.getElementById("element").style.color = "violet";
+
+
+
+//   var p = Home.prototype = createObject(Section.prototype);
+
+//   function Home( home) {
+// 		Section.apply(this, arguments);
+// 	};
+
+// 	p.init = function( root ) {
+// 		console.log('@home init');
+// 	    Section.prototype.init.call(this, arguments);
+// 	};
+   
+
+//     function show_more_menu(e) {
+//         if( !confirm(`Go to ${e.target.href} ?`) ) e.preventDefault();
+//       }
+
+
